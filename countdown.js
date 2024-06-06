14b6e91c5caeb889885e9937b88b8c2df33ecff3
Natan Pivetta <51028072+NatanPivetta@users.noreply.github.com>
2024-06-06 17:55:20 -0300

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,56 @@
+var myInterval = setInterval(clock, 1000);
+
+function createButton() {
+  var element = document.getElementById("countdown");
+  element.remove();
+  var button = document.createElement('button');
+  button.type = 'button';
+  button.className = 'heart';
+  button.setAttribute('onClick', 'homePage()');
+
+  var container = document.getElementById('banner');
+  container.appendChild(button);
+}
+
+function homePage() {
+  window.open("https://f324bf8b-d714-420f-bd7c-2233f9266a97-00-1a7k6w7rvym3x.kirk.replit.dev/quiz.html");
+}
+
+if (clock() <= 0) {
+  var stop = true;
+
+}
+
+if (stop === true) {
+
+  createButton();
+  console.log(clock());
+}
+
+function clock() {
+  var startDate = new Date().getTime();
+  // var endDate = new Date();
+  var endDate = new Date("2024-05-03T21:29:59");
+  // let endDate = new Date("2023-01-26T10:29:00");
+  var timeRemaining = parseInt((endDate - startDate) / 1000);
+  if (timeRemaining >= 0) {
+    days = parseInt(timeRemaining / 86400);
+    timeRemaining = (timeRemaining % 86400);
+    hours = parseInt(timeRemaining / 3600);
+    timeRemaining = (timeRemaining % 3600);
+    minutes = parseInt(timeRemaining / 60);
+    timeRemaining = (timeRemaining % 60);
+    seconds = parseInt(timeRemaining);
+    document.getElementById("days").innerHTML = parseInt(days, 10) + ":";
+    document.getElementById("hours").innerHTML = ("0" + hours).slice(-2) + ":";
+    document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2) + ":";
+    document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+  }
+  return timeRemaining;
+
+}
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,56 @@
+var mes = (new Date().getMonth());
+
+var tentativas = ["sabor", "olhos", "arms", "gatinha", "algema", "sapinha", "baby", "love", "smile", "sol"];
+
+var tentativas24 = ["", "", "paint", "", "sapinha", "burning"];
+
+
+
+function mostraBarraSubmit() {
+  var disp = document.getElementById("barraSubmit").style.display;
+  if (disp === "block") {
+    disp = "none";
+  } else {
+    disp = "block";
+  }
+}
+
+function submitPalavra() {
+  let palavraDigitada = document.getElementById("palavraDigitada").value;
+  console.log(palavraDigitada);
+  console.log(tentativas[mes], mes);
+  palavraDigitada = palavraDigitada.toLowerCase();
+  console.log(palavraDigitada);
+  if (palavraDigitada == tentativas24[mes]) {
+    console.log("Acertou!");
+    r = confirm("Acertou!");
+    if (r) {
+      window.location.href = "https://f324bf8b-d714-420f-bd7c-2233f9266a97-00-1a7k6w7rvym3x.kirk.replit.dev/mes17.html";
+      console.log("confirmou");
+    }
+  } else {
+    alert("Errou! Tenta de novo");
+    document.getElementById("palavraDigitada").value = '';
+  }
+}
+
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+  let element = document.getElementById('embed-iframe');
+  let options = {
+    width: '100%',
+    height: '152',
+    uri: 'https://open.spotify.com/playlist/1Q7OFR10FTsBounh78s1Lh'
+  };
+  let callback = (EmbedController) => { };
+  IFrameAPI.createController(element, options, callback);
+
+};
+
+function cuzin() {
+  window.location.href = "https://www.youtube.com/watch?v=orWnzqBA63w";
+}
+
+function semCuzin() {
+  alert("Errou! Tenta de novo");
+  document.getElementById("botaonao").style.display = "none";
+}
